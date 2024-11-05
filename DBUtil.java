8ab850057c4bec7c5da6b55e0794378392c4743f
aditiii09sharma <129159271+aditiii09sharma@users.noreply.github.com>
2024-11-05 01:59:24 -0800

    Create DBUtil.java

---
+++ DBUtil.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/exam_db", "root", "password");
+    }
+}
